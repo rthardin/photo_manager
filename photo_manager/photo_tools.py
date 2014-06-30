f8b97ce00e5553900c457747a6847e3c61404650
Ryan Hardin <ryan.t.hardin@gmail.com>
2014-06-30 18:10:32 -0400

    Added some tests and stuff

--- photo_manager/photo_tools.py
+++ photo_manager/photo_tools.py
@@ -1,3 +1,5 @@
+import shutil
+import errno
 import os
 import exifread
 from datetime import datetime
@@ -6,7 +8,11 @@ from datetime import datetime
 def get_metadata(path):
     # Open the image and retrieve all of the metadata
     with open(path, 'rb') as img_file:
-        return exifread.process_file(img_file)
+        metadata = exifread.process_file(img_file)
+    # If it doesn't contain metadata, raise an Exception
+    if not metadata:
+        raise ValueError('%r is not an image, or does not contain EXIF metadata' % path)
+    return metadata
 
 
 def get_datetime(path):
@@ -17,6 +23,34 @@ def get_datetime(path):
         metadata = exifread.process_file(img_file, details=False, stop_tag=datetime_tag)
     # Get the datetime string
     if not metadata.get(datetime_tag):
-        return None
+        raise ValueError('%r is not an image, or does not contain EXIF metadata' % path)
     # Convert the datetime str into a datetime
     return datetime.strptime(str(metadata.get(datetime_tag)), '%Y:%m:%d %H:%M:%S')
+
+
+def organize(input_root, output_root):
+    moved_files = []
+    # Find all the photos in the input_dir
+    for dirpath, dirnames, filenames in os.walk(input_root):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            try:
+                # Get the EXIF datetime
+                dt = get_datetime(filepath)
+                # Create the destination directory, if it does not exist
+                output_dirs = os.path.join(output_root, str(dt.year), str(dt.month))
+                try:
+                    os.makedirs(output_dirs)
+                except OSError as e:
+                    if e.errno != errno.EEXIST:
+                        raise e
+                # Move the image to the destination
+                destination_path = os.path.join(output_dirs, filename)
+                shutil.move(filepath, destination_path)
+                # Append the paths to the list of moved files
+                moved_files.append({'source': filepath,
+                                    'destination': destination_path})
+            # If it's not an image, or does not contain EXIF metadata, skip it
+            except ValueError:
+                continue
+    return moved_files
--- test/test_photo_tools.py
+++ test/test_photo_tools.py
@@ -7,32 +7,38 @@ from photo_manager import photo_tools
 
 
 photo_directory = './resource/images'
+valid_photo_directory = './resource/images/valid'
 
 
 class TestPhotoTools(unittest.TestCase):
 
     def setUp(self):
+        # Test directory
+        self.test_dir = self.__class__.__name__
         # Delete the test directory if it exists
         try:
-            shutil.rmtree(self.__class__.__name__)
+            shutil.rmtree(self.test_dir)
         except OSError:
             pass
         # Create the test directory
-        os.mkdir(self.__class__.__name__)
+        os.mkdir(self.test_dir)
 
     def tearDown(self):
         # Delete the test directory if it exists
         try:
-            shutil.rmtree(self.__class__.__name__)
+            shutil.rmtree(self.test_dir)
         except OSError:
             pass
 
     def test_get_metadata(self):
+        # What happens if a non-photo is attempted?
+        with self.assertRaises(ValueError):
+            photo_tools.get_metadata('test_photo_tools.py')
         # Get a list of photos
-        photo_filenames = os.listdir(photo_directory)
+        photo_filenames = os.listdir(valid_photo_directory)
         # Iterate over each, and check for metadata
         for photo_filename in photo_filenames:
-            photo_path = os.path.join(photo_directory, photo_filename)
+            photo_path = os.path.join(valid_photo_directory, photo_filename)
             metadata = photo_tools.get_metadata(photo_path)
             assert metadata, \
                 'get_metadata() should return a dict, not %s ' % type(metadata)
@@ -40,14 +46,29 @@ class TestPhotoTools(unittest.TestCase):
                 'get_metadata() returned a dict with no keys'
 
     def test_get_datetime(self):
+        # What happens if a non-photo is attempted?
+        with self.assertRaises(ValueError):
+            photo_tools.get_datetime('test_photo_tools.py')
         # Get a list of photos
-        photo_filenames = os.listdir(photo_directory)
+        photo_filenames = os.listdir(valid_photo_directory)
         # Iterate over each, and check for a correct datetime
         for photo_filename in photo_filenames:
-            photo_path = os.path.join(photo_directory, photo_filename)
+            photo_path = os.path.join(valid_photo_directory, photo_filename)
             dt = photo_tools.get_datetime(photo_path)
             assert isinstance(dt, datetime), \
                 'get_datetime() should return a datetime, not %s' % type(dt)
+
+    def test_organize_photos(self):
+        # Define and create the input and output directories
+        input_dir = os.path.join(self.test_dir, 'input')
+        output_dir = os.path.join(self.test_dir, 'output')
+        os.mkdir(input_dir)
+        os.mkdir(output_dir)
+        # Copy the test photos to the input directory
+        dirname = os.path.split(photo_directory)[1]
+        shutil.copytree(photo_directory, os.path.join(input_dir, dirname))
+        # Organize the photos
+        print photo_tools.organize(input_dir, output_dir)
 
 if __name__ == '__main__':
     unittest.main()
